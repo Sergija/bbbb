15ec15c752855a3c87b0efe941e0375c89a6b2e6
Smm <sergimlt@gmail.com>
2017-10-15 22:58:19 +0200

    First commit, just tot try!!

---
+++ app/src/main/java/com/app/smm/m15firstapp/Pregunta.java
@@ -0,0 +1,19 @@
+package com.app.smm.m15firstapp;
+
+/**
+ * Created by Smm on 09/10/2017.
+ */
+
+public class Pregunta {
+    private boolean answer;
+    private int questionId;
+    private int pistaId;
+
+    public Pregunta (boolean answer, int questionId, int pistaId){
+
+        this.answer=answer;
+        this.questionId=questionId;
+        this.pistaId=pistaId;
+    }
+}
+
